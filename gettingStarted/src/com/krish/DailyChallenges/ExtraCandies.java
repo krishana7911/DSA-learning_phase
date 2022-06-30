f3550f158745c1f70b7a37f0387fd172bf573ee6
krishana7911 <krishanagautam7911@gmail.com>
2022-06-30 12:10:38 +0530

    Returned in List<Boolean>

--- gettingStarted/src/com/krish/DailyChallenges/ExtraCandies.java
+++ gettingStarted/src/com/krish/DailyChallenges/ExtraCandies.java
@@ -1,19 +1,23 @@
 package com.krish.DailyChallenges;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExtraCandies {
     public static void main(String[] args) {
         int[] candies = {2, 4, 5, 1, 3};
         int extraCandies = 1;
-        boolean[] ans = maxCandies(candies, extraCandies);
+        List<Boolean> ans = maxCandies(candies, extraCandies);
 
-        for(int i = 0; i < ans.length; i++) {
-            System.out.print(ans[i] + " ");
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
         }
     }
 
-    static boolean[] maxCandies(int[] candies, int extraCandies) {
+    static List<Boolean> maxCandies(int[] candies, int extraCandies) {
         //we need a variable to store the max number of candies
-        boolean[] trueOrNot = new boolean[candies.length];
+        //boolean[] trueOrNot = new boolean[candies.length];
+        List<Boolean> list = new ArrayList<Boolean>();
 
         int maxNoCandie = candies[0];
         for (int i = 1; i < candies.length; i++) {
@@ -25,10 +29,13 @@ public class ExtraCandies {
         for (int j = 0; j < candies.length; j++) {
             candies[j] += extraCandies;
             if (candies[j] >= maxNoCandie) {
-                trueOrNot[j] = true;
+                //trueOrNot[j] = true;
+                list.add(true);
+            } else {
+                list.add(false);
             }
         }
 
-        return trueOrNot;
+        return list;
     }
 }
